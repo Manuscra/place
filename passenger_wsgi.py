d449e510805c0b130c426de7556a62a3ca5b525b
Manu <manu@Coyote.(none)>
2026-05-19 22:49:00 +0200

    create .env

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,9 +1,11 @@
 import os
 import sys
 
+# Toujours en premier : path et env
 sys.path.insert(0, os.path.dirname(__file__))
+os.environ.setdefault("FLASK_ENV", "production")
+os.environ.setdefault("SECRET_KEY", "changez-moi-en-production-12345")
 
 from src.app import create_app
 
-os.environ.setdefault("FLASK_ENV", "production")
 application = create_app()
