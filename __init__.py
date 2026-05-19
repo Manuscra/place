dd1d9659ea88e16d1cb9a17ace8a5a74b20886b4
Manu <manu@Coyote.(none)>
2026-05-19 23:05:06 +0200

    use init

--- __init__.py
+++ __init__.py
@@ -1,11 +1,10 @@
 import os
 import sys
 
-# Toujours en premier : path et env
 sys.path.insert(0, os.path.dirname(__file__))
 os.environ.setdefault("FLASK_ENV", "production")
 os.environ.setdefault("SECRET_KEY", "changez-moi-en-production-12345")
 
 from src.app import create_app
 
-application = create_app()
+app = create_app()
